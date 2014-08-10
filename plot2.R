2a4105757b17c64ad85760e533f52380e69b3d70
Benjamin Wang <wangyuanyu@gmail.com>
2014-08-10 09:25:50 -0400

    add plot2
    source and graph commit.

--- plot2.R
+++ plot2.R
@@ -5,12 +5,11 @@ rows<-grepl("^[12]/2/2007",data$Date)
 data<-data[rows,]
 
 #convert column to right format
-data<-transform(data, Date = as.Date(as.character(Date)))
-data<-transform(data, Time = strptime(Time, format="%H:%M:%S"))
+data$Date2 <- strptime(paste(data$Date,data$Time), "%d/%m/%Y %H:%M:%S")
 data<-transform(data, Global_active_power = as.numeric(Global_active_power))
 
 #drawgraph
 #dev.set(dev.prev())
-png(filename = "plot1.png", width = 480, height = 480, units = "px")
-hist(data$Global_active_power, main="Global Active Power",xlab="Global Active Power (kilowatts)", col="red")
+png(filename = "plot2.png", width = 480, height = 480, units = "px")
+plot(data$Date2, data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
 dev.off()
